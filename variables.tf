ea0812a728d19108a6595dd11297021fc1cd8a72
fafnirical <fafnirical@users.noreply.github.com>
2020-08-23 11:18:21 -0700

    Validate AWS region variable

--- variables.tf
+++ variables.tf
@@ -2,6 +2,34 @@ variable "region" {
   type        = string
   description = "The AWS region to launch the cloud gaming instance in"
   default     = "us-west-2"
+  validation {
+    condition = contains(
+      [
+        "us-east-1",
+        "us-east-2",
+        "us-west-1",
+        "us-west-2",
+        "ca-central-1",
+        "eu-central-1",
+        "eu-west-1",
+        "eu-west-2",
+        "eu-west-3",
+        "eu-north-1",
+        "eu-south-1",
+        "ap-east-1",
+        "ap-southeast-1",
+        "ap-southeast-2",
+        "ap-northeast-2",
+        "ap-northeast-1",
+        "ap-south-1",
+        "sa-east-1",
+        "me-south-1",
+        "af-south-1",
+      ],
+      var.region
+    )
+    error_message = "The region value must be a valid AWS region."
+  }
 }
 
 variable "amis" {
